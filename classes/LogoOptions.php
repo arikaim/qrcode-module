c78a3665e6d369c32863ffd3c5645669a54b0a57
Arikaim <sales@intersoft.biz>
2021-07-20 18:17:57 +0300

    =

--- classes/LogoOptions.php
+++ classes/LogoOptions.php
@@ -8,14 +8,44 @@
  * 
 */
 namespace Arikaim\Modules\Qrcode\Classes;
-
 use chillerlan\QRCode\QROptions;
+use chillerlan\QRCode\QRCode;
 
 /**
  * Qr code logo image options
  */
 class LogoOptions extends QROptions
 {
+    /**
+     * space height
+     *
+     * @var int
+     */
     protected $logoSpaceWidth;
-	protected $logoSpaceHeight;
+	
+    /**
+     * Space width
+     *
+     * @var int
+     */
+    protected $logoSpaceHeight;
+
+    /**
+     * Constructor
+     *
+     * @param array|null $options
+     */
+    public function __construct(?array $options = null)
+    {
+        $this->eccLevel = QRCode::ECC_H;
+
+        if (\is_array($options) == true) {
+            $this->version = $options['version'] ?? 7;            
+            $this->imageBase64 = $options['imageBase64'];
+            $this->logoSpaceWidth   = $options['space_width'] ?? 10;
+            $this->logoSpaceHeight  = $options['space_height'] ?? 10;
+            $this->scale            = $options['scale'] ?? 5;
+            $this->imageTransparent = $options['transparent'] ?? false;
+        }
+    } 
 }
